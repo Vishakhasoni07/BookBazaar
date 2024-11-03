1d59f4789fc75875c69affe508521a18564bf200
harsha-devstar <harshithadeveloper20@gmail.com>
2024-11-03 23:00:23 +0530

    updated

--- script.js
+++ script.js
@@ -218,7 +218,7 @@ for(let i=0;i<add_cart_btns.length;i++)
     
     add_cart_btns[i].addEventListener('click', ()=>{
         books_in_cart.push(books_in_buy[i]);
-        
+        add_cart_btns[i].disabled = true;
         
         localStorage.setItem('books_in_cart', JSON.stringify(books_in_cart));
     })
@@ -231,15 +231,16 @@ for(let i=0;i<add_cart_btns.length;i++)
 // localStorage.setItem('books_in_cart', JSON.stringify(books_in_cart_ls));})
 
 // display books in cart
-
+let total_price = 0.0;
+const total_price_el = document.querySelector('#total-price');
 const products = document.querySelector('.products');
 if (products) {
-    const books_in_cart_ls = JSON.parse(localStorage.getItem('books_in_cart')) || [];
-    let total_price = 0.0;
+    // const books_in_cart_ls = JSON.parse(localStorage.getItem('books_in_cart')) || [];
+    
     books_in_cart_ls.forEach(book => {
         total_price += Number(book.price);
         const product = document.createElement('div');
-        product.classList.add('book-card');
+        product.classList.add('product');
         
         product.innerHTML = `
           
@@ -249,12 +250,51 @@ if (products) {
             <div class="book-price">Price: $${book.price}</div>
             <div class="book-seller">Seller: ${book.seller.name}</div>
             <div class="book-phone">Contact: ${book.seller.phone}</div>
-            
+            <div><button class="remove-from-cart">Remove</button></div>
             
         `;
         products.appendChild(product);
 
     });
-    const total_price_el = document.querySelector('#total-price');
+    
     total_price_el.innerHTML = `Total : $${total_price}`;
+}
+const product_els = document.querySelectorAll('.product');
+let remove_btns = document.querySelectorAll('.remove-from-cart');
+// const books_in_cart_ls = JSON.parse(localStorage.getItem('books_in_cart')) || [];
+
+for(let i =0;i<remove_btns.length;i++)
+{
+    remove_btns[i].addEventListener('click', ()=>{
+    
+    product_els[i].innerHTML = '';
+    product_els[i].style.border = 'none';
+    // document.remove(product_els[i]);
+    let books_in_cart_ls = JSON.parse(localStorage.getItem('books_in_cart')) || [];
+    // delete books_in_cart_ls[i];
+    total_price -= Number(books_in_cart_ls[i].price);
+    total_price_el.innerHTML = `Total : $${total_price}`;
+    books_in_cart_ls.splice(i,1);
+    
+    localStorage.setItem('books_in_cart', JSON.stringify(books_in_cart_ls));
+    
+    // let total_price = 0.0;
+    // books_in_cart_ls.forEach(book => {
+    //     total_price += Number(book.price);
+    //     const product = document.createElement('div');
+    //     product.classList.add('product');
+        
+    //     product.innerHTML = `
+          
+    //         <div class="book-title">${book.title}</div>
+    //         <div class="book-author">by ${book.author}</div>
+    //         <div class="book-condition">Condition: ${book.condition}</div>
+    //         <div class="book-price">Price: $${book.price}</div>
+    //         <div class="book-seller">Seller: ${book.seller.name}</div>
+    //         <div class="book-phone">Contact: ${book.seller.phone}</div>
+    //         <div><button class="remove-from-cart">Remove</button></div>
+            
+    //     `;
+    //     products.appendChild(product);})
+    })
 }
